e367fae2e170afe8de713a3a9618af47554091f0
yusufhoruz2020 <69108776+yusufhoruz2020@users.noreply.github.com>
2020-08-02 13:42:20 +0200

    Update TestClass.java

--- src/TestClass.java
+++ src/TestClass.java
@@ -3,6 +3,8 @@ public class TestClass {
     public static void main(String[] args) {
 
         System.out.println("Git den merhaba");
+        
+        System.out.println("GitHub dan merhaba");
     }
 
 
